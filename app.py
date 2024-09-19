23b858f8f0a7fe368ebeb769f7ec04044d02f94d
Harsha S <sharsha315@gmail.com>
2024-09-19 13:28:52 +0000

    Added Boilerplate code and necessary files to get started

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+
+
+
+
+def main():
+    st.set_page_config("AskTheDoc")
+    st.header("AskTheDoc - Learn Smartly with Documents using Gemini💁")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
